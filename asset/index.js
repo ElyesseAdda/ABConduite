fe13518eba2dccfa7b366448d5bce9287933ae8f
Wlhtumadeurh <Elyesseadda@hotmail.com>
2023-05-18 13:27:36 +0200

    Format Mobile terminer

---
+++ asset/index.js
@@ -0,0 +1,27 @@
+const sliderPosition = document.querySelector(".slider-forfait")
+const bNavMobile = document.querySelector(".btn-navmobile")
+const navMobile = document.querySelector(".nav-mobile")
+const crossMobileNav = document.querySelector(".fa-xmark")
+basePosition = 0
+
+
+  const slider = () => {  setTimeout(() =>{
+        if( basePosition < 900)
+        {basePosition = basePosition +5
+        sliderPosition.style.right = basePosition +"px"
+        console.log(basePosition);}
+        else if (basePosition >= 900){ 
+            basePosition = 0
+        }
+
+    },100)}
+    setInterval(slider,100);
+
+ bNavMobile.addEventListener("click", () =>{
+    navMobile.style.left = "0"
+ })
+
+ crossMobileNav.addEventListener("click", () =>{
+    navMobile.style.left = "-100%"
+ })
+
